5536aa8eb38b4706745e6ec21d1bf0cb1cc89d10
Aniket Maurya <aniketmaurya@outlook.com>
2021-11-28 00:54:16 +0530

    update

--- examples/HF-Spaces-gradsflow-text-classification/app.py
+++ examples/HF-Spaces-gradsflow-text-classification/app.py
@@ -1,5 +1,4 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
-
 from chitra.serve.app import GradioApp
 
 tokenizer = AutoTokenizer.from_pretrained(
@@ -8,9 +7,7 @@ model = AutoModelForSequenceClassification.from_pretrained(
     "finiteautomata/beto-sentiment-analysis")
 classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
-
-app = GradioApp(
-    "text-classification",
-    model=classifier,
-)
+app = GradioApp("text-classification",
+                model=classifier,
+                desc="🤗 HuggingFace Sentiment Analysis Example with CHITRA")
 app.run()
